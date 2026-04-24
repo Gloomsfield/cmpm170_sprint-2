d9c60b50b9514f7e4ca15fb9d7ec492f6718aa23
Gloomsfield <amory@acosta.boo>
2026-04-24 12:57:17 -0700

    add skeleton DogBarkState

--- src/gameObjects/characters/Dog.js
+++ src/gameObjects/characters/Dog.js
@@ -1,9 +1,51 @@
 import { Enemy } from "./Enemy.js";
+import { State } from "@lib/StateMachine.js"
 
 export default class Dog extends Enemy {
 
     constructor(scene, x, y, properties) {
         super(scene, x, y, 'dog_texture', 0, properties);
+
+		console.log(this.fsm);
+
+		this.fsm.transition('bark');
     }
 
+	initializeStates() {
+		return {
+			'idle': new DogIdleState(),
+			'bark': new DogBarkState(),
+		};
+	}
+
+	bark() {
+		this.play('dog_bark_anim');
+		
+		const barkSound = this.scene.sound.add('dog_bark_sound');
+		barkSound.on('complete', () => { this.fsm.transition('idle'); }, this);
+
+		barkSound.play();
+	}
+
 }
+
+class DogIdleState extends State {
+	enter(scene, dogObject) {
+
+	}
+
+	execute(scene, dogObject) {
+		
+	}
+}
+
+class DogBarkState extends State {
+	enter(scene, dogObject) {
+		dogObject.bark();
+	}
+
+	execute(scene, dogObject) {
+		
+	}
+}
+
