c30e30e86f5d96822c9499480d44efa485d68aa4
Gloomsfield <amory@acosta.boo>
2026-04-24 12:07:47 -0700

    load baby animations

--- src/scenes/Initialize.js
+++ src/scenes/Initialize.js
@@ -6,9 +6,50 @@ export class Initialize extends Phaser.Scene {
     }
 
     preload() {
+		this.load.path = './assets/baby/';
+
+		this.load.image('baby_spritesheet', 'baby.webp', {
+			frameWidth: 16, frameHeight: 16
+		});
+
+		this.anims.create({
+			key: 'baby_walk-forward_anim',
+			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
+				start: 0, end: 3,
+			}),
+			duration: 500,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: 'baby_walk-left_anim',
+			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
+				start: 4, end: 7,
+			}),
+			duration: 500,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: 'baby_walk-right_anim',
+			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
+				start: 4, end: 7,
+			}),
+			duration: 500,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: 'baby_walk-backward_anim',
+			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
+				frames: [ 8, 9, 8, 10, ],
+			}),
+			duration: 500,
+			repeat: -1,
+		});
+
         this.load.path = './assets/temp/';
 
-		this.load.image('baby_texture', 'dummy.png');
 		this.load.image('bear_texture', 'dummy.png');
 		this.load.image('clown_texture', 'dummy.png');
 		this.load.image('dog_texture', 'dummy.png');
@@ -17,11 +58,14 @@ export class Initialize extends Phaser.Scene {
 		this.load.image('moth_texture', 'dummy.png');
 		this.load.image('puppet_texture', 'dummy.png');
 
+		this.load.audio('dog_bark_sound', 'dog_bark_sound.mp3');
+
 		this.load.image('sawblade_texture', 'sawblade.png');
 
 		this.load.image('dungeon_tileset', 'tileset_toadzilla_dungeon.png');
 
 		this.load.tilemapTiledJSON('tutorial_tilemap', 'room1.json');
+		this.load.tilemapTiledJSON('amory_tilemap', 'amory-debug.json');
 
 		this.load.path = './assets/tilemaps/';
     }
