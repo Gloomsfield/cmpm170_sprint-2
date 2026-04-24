55f99c0a571228cbe380862472664b0c63197877
Gloomsfield <amory@acosta.boo>
2026-04-24 12:18:05 -0700

    move baby animation loading into Baby class

--- src/scenes/Initialize.js
+++ src/scenes/Initialize.js
@@ -8,44 +8,8 @@ export class Initialize extends Phaser.Scene {
     preload() {
 		this.load.path = './assets/baby/';
 
-		this.load.image('baby_spritesheet', 'baby.webp', {
+		this.load.spritesheet('baby_spritesheet', 'baby.webp', {
 			frameWidth: 16, frameHeight: 16
-		});
-
-		this.anims.create({
-			key: 'baby_walk-forward_anim',
-			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
-				start: 0, end: 3,
-			}),
-			duration: 500,
-			repeat: -1,
-		});
-
-		this.anims.create({
-			key: 'baby_walk-left_anim',
-			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
-				start: 4, end: 7,
-			}),
-			duration: 500,
-			repeat: -1,
-		});
-
-		this.anims.create({
-			key: 'baby_walk-right_anim',
-			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
-				start: 4, end: 7,
-			}),
-			duration: 500,
-			repeat: -1,
-		});
-
-		this.anims.create({
-			key: 'baby_walk-backward_anim',
-			frames: this.anims.generateFrameNumbers('baby_spritesheet', {
-				frames: [ 8, 9, 8, 10, ],
-			}),
-			duration: 500,
-			repeat: -1,
 		});
 
         this.load.path = './assets/temp/';
