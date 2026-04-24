e87eda12f420e7ffe8f4f5af11c41c25638df7cb
Gloomsfield <amory@acosta.boo>
2026-04-24 12:18:26 -0700

    load and play baby animations

--- src/gameObjects/characters/Baby.js
+++ src/gameObjects/characters/Baby.js
@@ -4,7 +4,45 @@ import { State } from "@lib/StateMachine.js";
 export default class Baby extends Character {
 
     constructor(scene, x, y) {
-        super(scene, x, y, 'baby_texture', 0);
+        super(scene, x, y, 'baby_spritesheet', 0);
+
+		this.scene.anims.create({
+			key: 'baby_walk-forward_anim',
+			frames: this.scene.anims.generateFrameNumbers('baby_spritesheet', {
+				frames: [ 0, 1, 2, 3 ]
+			}),
+			frameRate: 8,
+			repeat: -1,
+		});
+
+		this.scene.anims.create({
+			key: 'baby_walk-left_anim',
+			frames: this.scene.anims.generateFrameNumbers('baby_spritesheet', {
+				frames: [ 4, 5, 6, 7 ]
+			}),
+			frameRate: 8,
+			repeat: -1,
+		});
+
+		this.scene.anims.create({
+			key: 'baby_walk-right_anim',
+			frames: this.scene.anims.generateFrameNumbers('baby_spritesheet', {
+				frames: [ 4, 5, 6, 7 ]
+			}),
+			frameRate: 8,
+			repeat: -1,
+		});
+
+		this.scene.anims.create({
+			key: 'baby_walk-backward_anim',
+			frames: this.scene.anims.generateFrameNumbers('baby_spritesheet', {
+				frames: [ 8, 9, 8, 10, ],
+			}),
+			frameRate: 8,
+			repeat: -1,
+		});
+
+		this.play('baby_walk-forward_anim');
     }
 
     initializeStates() {
