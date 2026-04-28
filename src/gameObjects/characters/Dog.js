99d3e117b0e0750c676aa5a450feb8d7fb4ce9e2
Gloomsfield <amory@acosta.boo>
2026-04-27 18:15:17 -0700

    add Dog bark state

--- src/gameObjects/characters/Dog.js
+++ src/gameObjects/characters/Dog.js
@@ -1,9 +1,42 @@
 import { Enemy } from "./Enemy.js";
 
+import { State } from "@lib/StateMachine.js";
+
 export default class Dog extends Enemy {
 
     constructor(scene, x, y, properties) {
         super(scene, x, y, 'dog_texture', 0, properties);
     }
 
+	initializeStates() {
+		return {
+			'idle': new DogIdleState(),
+			'bark': new DogBarkState(),
+		};
+	}
+
+	bark() {
+		this.fsm.transition('bark');
+
+		this.play('dog_bark_anim');
+
+		const barkSound = this.scene.sound.add('dog_bark_sound');
+		barkSound.on('complete', () => { this.fsm.transition('idle'); }, this);
+
+		barkSound.play();
+	}
+
 }
+
+class DogIdleState extends State {
+	enter(scene, dogObject) {}
+
+	execute(scene, dogObject) {}
+}
+
+class DogBarkState extends State {
+	enter(scene, dogObject) { }
+
+	execute(scene, dogObject) {}
+}
+
