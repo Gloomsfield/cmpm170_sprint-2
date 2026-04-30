2cc92481abb551d8bfb1e13b4a2b1b3d3f360a2d
Gloomsfield <amory@acosta.boo>
2026-04-30 08:32:46 -0700

    create Dog's bark Effect

--- src/gameObjects/characters/Dog.js
+++ src/gameObjects/characters/Dog.js
@@ -1,11 +1,22 @@
 import { Enemy } from "./Enemy.js";
 
+import { EffectShape, EffectDescriptor, Effect } from "../effects/Effect.js";
+
 import { State } from "@lib/StateMachine.js";
 
 export default class Dog extends Enemy {
 
     constructor(scene, x, y, properties) {
         super(scene, x, y, 'dog_texture', 0, properties);
+
+		this.barkEffectDescriptor = new EffectDescriptor(
+			'bark',
+			EffectShape.createCircle(30),
+			(object1, object2) => { console.log(`${object1} collided with ${object2}`); },
+			100
+		);
+
+		this.bark();
     }
 
 	initializeStates() {
@@ -23,6 +34,8 @@ export default class Dog extends Enemy {
 		const barkSound = this.scene.sound.add('dog_bark_sound');
 		barkSound.on('complete', () => { this.fsm.transition('idle'); }, this);
 
+		const barkEffect = new Effect(this.scene, this.x, this.y, this.barkEffectDescriptor);
+
 		barkSound.play();
 	}
 
@@ -35,7 +48,7 @@ class DogIdleState extends State {
 }
 
 class DogBarkState extends State {
-	enter(scene, dogObject) { }
+	enter(scene, dogObject) {}
 
 	execute(scene, dogObject) {}
 }
