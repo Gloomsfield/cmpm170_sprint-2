94ff7dd61af88ee41aacb4a5fc72f6f1d81a12a6
Gloomsfield <amory@acosta.boo>
2026-04-30 22:39:18 -0700

    move Puppet along path

--- src/gameObjects/characters/Puppet.js
+++ src/gameObjects/characters/Puppet.js
@@ -1,9 +1,29 @@
 import { Enemy } from "./Enemy.js";
+import { State } from "@lib/StateMachine.js";
+import { offsetPolylinePath } from "@src/tiledImport.js";
 
 export default class Puppet extends Enemy {
 
     constructor(scene, x, y, properties) {
         super(scene, x, y, 'puppet_texture', 0, properties);
+
+		this.pathGoals = properties.path ? offsetPolylinePath(properties.path) : [];
+		this.pathGoals.shift();
+		this.moveToNextGoal();
     }
 
+	initializeStates() {
+		return {
+			idle: new PuppetIdleState(),
+		};
+	}
+
 }
+
+class PuppetIdleState extends State {
+	
+	enter(scene, puppetObject) {}
+	execute(scene, puppetObject) {}
+
+}
+
