f8058aae9a12ca8af5a13b09594c73c634ec6041
edjones079 <electrickazoo@proton.me>
2026-05-01 10:15:43 -0700

    Saw Blade Implemented

--- src/gameObjects/traps/Sawblade.js
+++ src/gameObjects/traps/Sawblade.js
@@ -35,7 +35,6 @@ export default class Sawblade extends Trap {
 
 		});
 
-<<<<<<< HEAD
 		const ambientSawSound = this.scene.sound.add('saw_sound_ambient', {loop: true, volume: 0.2});
 
 	
@@ -45,21 +44,6 @@ export default class Sawblade extends Trap {
 
 	whirr() {
 
-=======
-		const color = 0xFF_66_00;
-		const colorOver = 0xFF_FF_00;
-		
-
-		this.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
-			this.glow.color = colorOver;
-		}, this);
-
-		this.on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
-			this.glow.color = color;
-		}, this);
-
-        this.glow = this.postFX.addGlow(color, 3, 0, false, 0.1, 3);
->>>>>>> origin
 	}
 
 }
